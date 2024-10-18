f32bafc8742cf7482ce7ea10a1fb7f53e2e254db
Christ <jeanchristkoko23@gmail.com>
2024-10-17 22:20:05 -0400

    ajout de revue

--- AppConsoleLivres/Program.cs
+++ AppConsoleLivres/Program.cs
@@ -1,7 +1,16 @@
-﻿internal class Program
+﻿using AppConsoleLivres;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Revue revue1 = new Revue ("Sport&Sante", "Cesar DUBOIS", 60, 2, 2015);
+        Revue revue2 = new Revue ("Passion", "Kader Diop", 150, 7, 2020);
+        Revue revue3 = new Revue ("LeMonde", "Eliel Kouakou", 200, 4, 2023);
+
+        revue1.AfficherDetails(); 
+        revue2.AfficherDetails(); 
+        revue3.AfficherDetails();
+
     }
 }
---
+++ AppConsoleLivres/Revue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConsoleLivres
+{
+    internal class Revue : Livre
+    {
+        public int Numero { get; set; }
+        public int Annee { get; set; }
+
+        public Revue(string titre, string auteur, int nbrPages, int numero, int annee) : base (titre, auteur, nbrPages)
+        {
+            Numero = numero;
+            Annee = annee;
+        }
+
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"La revue numero {Numero} de l'auteur {Auteur} qui a pour titre {Titre} est sortie en {Annee} et compte {NombrePages} page.") ;
+        }
+    }
+}
