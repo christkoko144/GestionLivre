274f84cbefaa1c1ff1622154d6364374b926cf82
Christ <jeanchristkoko23@gmail.com>
2024-10-17 20:55:46 -0400

    ajout livre

---
+++ AppConsoleLivres/Livre.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConsoleLivres
+{
+    internal class Livre
+    {
+        public string Titre { get; set; }
+        public string Auteur { get; set; }
+        public int NombrePages { get; set; }
+
+        public Livre(string titre, string auteur, int nbrPages) {
+            Titre = titre;
+            Auteur = auteur;
+            NombrePages = nbrPages;
+        }
+
+        public virtual void AfficherDetails()
+        {
+            Console.WriteLine($"Le livre {Titre} est de l'auteur {Auteur} et contient {NombrePages} pages");
+            
+        }
+    }
+    
+}
